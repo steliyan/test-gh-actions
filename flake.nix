4313748b189f37c8a7b451976d20d8915f61a76d
Steliyan Stoyanov <steliyan@st6.io>
2020-12-23 09:09:21 +0200

    Add dummy workflows

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.actionlint
+              pkgs.yq-go
+            ];
+          };
+        }
+      );
+    };
+}
