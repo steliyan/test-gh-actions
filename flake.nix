045561566a0cc311a9d802a3ffaf692b2f9de67a
Steliyan Stoyanov <steliyan@st6.io>
2025-10-28 15:23:25 +0200

    SAVEPOINT

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "flaky";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.jq
+              pkgs.jsonschema
+              pkgs.yq
+            ];
+          };
+        }
+      );
+    };
+}
